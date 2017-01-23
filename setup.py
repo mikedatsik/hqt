6f87dc2bcebfb50edd99531e87a628451bba32bf
mikedatsik <mikedatsik@gmail.com>
2017-01-23 12:42:26 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hqt',
+    version='2.0.3',
+    description='PyQt\PySide helper for Houdini',
+    url='https://github.com/mikedatsik/hqt',
+    author='paulwinex',
+    license='Apache License (2.0)',
+    py_modules=["appdirs"]
+)
